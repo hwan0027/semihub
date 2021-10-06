cba1bfc1d02df749574f1dac7f9b1c1b6438086c
geun1234 <91867805+geun1234@users.noreply.github.com>
2021-10-06 12:40:41 +0900

    Add files via upload

--- git_semitest/src/DBConn.java
+++ git_semitest/src/DBConn.java
@@ -1,36 +1,48 @@
-package customer;
+package movieMember;
 
 import java.sql.Connection;
+
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConn {
-	public static void main(String[] args) {
-		//connect api(JDBC)를 이용하여 mysql에 접속해보자.
-		try {
-			//디비에 접속하기 위해서 Driver클래스를 로드
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("클래스로드!");
-			
-			//mysql에 접속하기 위한 id, pass를 변수로 준비
-			String id = "root";
-			String password = "mysql";
-			
-			//mysql 접속 url을 셋팅
-			//프로토콜://아이피주소:포트번호/디비이름?파라미터
-			String url = "jdbc:mysql://localhost:3306/employee_db?characterEncoding=utf-8&serverTimezone=Asia/Seoul";
-			
-			//디비에 접속해보자
-			//디비에 접속하고 접속한 (connection)객체를 반환받는다.
-			Connection conn = DriverManager.getConnection(url,id,password);
-			
-			System.out.println("접속 성공");
-			System.out.println(conn);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
 
+public class DBConn {
+	private static Connection dbConn;	
+	public static Connection getConnection(){
+		if(dbConn==null){
+			try {
+				String url = "jdbc:mysql://localhost:3306/employee_db?characterEncoding=utf-8&serverTimezone=Asia/Seoul";
+				String user = "root";
+				String password = "mysql";
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+
+	         
+	            dbConn = DriverManager.getConnection(url, user, password);	
+			
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+				
+			}
+		return dbConn;
+		}
+		
+	public static void close(){
+		if(dbConn!=null){
+			try {
+				if(!dbConn.isClosed()){
+					dbConn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		//연결을 끊어주면 그 안에 쓰레기 값이 남아 두번째 연결을 할때는 Adapter에러가 발생, 접속이 안된다
+
+		//★항상 연결을 끊으면 dbConn을 null로 초기화 해야함
+		dbConn=null;
+	}
 }
