dd119a8c65110e4893bab67323b4b24eb46b6d32
kona0 <91867831+kona0@users.noreply.github.com>
2021-10-06 11:40:46 +0900

    Add files via upload
     회원관리

---
+++ git_semitest/src/CustomerMain.java
@@ -0,0 +1,51 @@
+package customer;
+
+import java.util.Scanner;
+
+import conning.DBConn;
+
+public class CustomerMain {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Customer ob = new Customer();
+
+		int ch;
+
+		while(true){
+
+			do{
+				System.out.print("1.입력  2.수정  3.탈퇴  4.회원전체출력  5.아이디검색  6.종료");
+				System.out.print("\n-------------------------------------\n▶");
+				ch = sc.nextInt();
+			} while(ch<1 || ch>6);
+
+			System.out.println();
+
+			switch(ch){
+
+			case 1:
+				ob.insert();
+				System.out.println(); break;
+			case 2:
+				ob.update();
+				System.out.println(); break;
+			case 3:
+				ob.delete();
+				System.out.println(); break;
+			case 4:
+				ob.selectAll();
+				System.out.println(); break;
+			case 5:
+				ob.searchId();
+				System.out.println(); break;
+			case 6:
+				DBConn.close();
+				System.exit(0);
+			}
+
+		}
+
+	}
+
+
+}
